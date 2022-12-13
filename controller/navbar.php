67156b829e339a547a07e4b61019415e74602875
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2022-12-12 22:41:41 -0500

    version 0

--- controller/navbar.php
+++ controller/navbar.php
@@ -7,19 +7,49 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.html" class="nav-item nav-link active">Home</a>
-                <a href="about.html" class="nav-item nav-link">About</a>
-                <a href="courses.html" class="nav-item nav-link">Courses</a>
+                <a href="index.php" class="nav-item nav-link active">Inicio</a>
                 <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Institución</a>
                     <div class="dropdown-menu fade-down m-0">
-                        <a href="team.html" class="dropdown-item">Our Team</a>
-                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                        <a href="404.html" class="dropdown-item">404 Page</a>
+                        <a href="nosotros.php" class="dropdown-item">Corporación CENDI</a>
+                        <a href="politicasDeCalidad.php" class="dropdown-item">Políticas de calidad</a>
+                        <a href="trabajeConNosotros.php" class="dropdown-item">Trabaje con nosotros</a>
+                        <a href="TramiteActualizacion.php" class="dropdown-item">Trámite actualización régimen tributario especial</a>
+                        <a href="/docs/MANUAL DE CONVIVENCIA ACTUALIZADO 2020.pdf" class="dropdown-item" download="Manual de convicencia CENDI 2020 Bachiller">Manual de convivencia Bachillerato</a>
+                        <a href="/docs/MA04 Manual de Convivencia - Versión 6.pdf" class="dropdown-item" download="Manual de convicencia CENDI 2020 Tecnicos">Manual de convivencia Técnicos</a>
                     </div>
                 </div>
-                <a href="contact.html" class="nav-item nav-link">Contact</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Programas técnicos</a>
+                    <div class="dropdown-menu fade-down m-0">
+                        <a href="auxiliarContable.php" class="dropdown-item">Auxiliar Contable</a>
+                        <a href="auxiliarAdm.php" class="dropdown-item">Auxiliar Administrativo</a>
+                        <a href="auxiliarMedico.php" class="dropdown-item">Técnico laboral como secretaria (o) médica (o)</a>
+                        <a href="auxiliarSecretariado.php" class="dropdown-item">Técnico laboral como secretaria (o) general</a>
+                        <a href="auxiliarRecursoHumanos.php" class="dropdown-item">Auxiliar en Recursos Humanos</a>
+                        <a href="auxiliarFarmaceutico.php" class="dropdown-item">Auxiliar en Servicios Farmacéuticos</a>
+                        <a href="auxiliarSalud.php" class="dropdown-item">Auxiliar en Administrativo en Salud</a>
+                        <a href="auxiliarServicioCliente.php" class="dropdown-item">Auxiliar en Servicio al Cliente y Telemercadeo</a>
+                        <a href="auxiliarSistemas.php" class="dropdown-item">Técnico laboral como auxiliar de sistemas informáticos</a>
+                        <a href="recepcionhotelera.php" class="dropdown-item">Técnico Laboral como Empleado de Recepción Hotelera</a>
+                        <a href="desarrolloSoftware.php" class="dropdown-item">Técnico Laboral como Asistente en Desarrollo de Software</a>
+                        <a href="ingles.php" class="dropdown-item">Conocimientos Académicos en inglés</a>
+                    </div>
+                </div>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Seminarios</a>
+                    <div class="dropdown-menu fade-down m-0">
+                        <a href="soporteVital.php" class="dropdown-item">Primeros Auxilios - Soporte vital básico</a>
+                        <a href="tics.php" class="dropdown-item">Tic´s</a>
+                        <a href="facturacionSalud.php" class="dropdown-item">Facturación en Salud</a>
+                        <a href="aseoDomestico.php" class="dropdown-item">Técnicas de aseo doméstico</a>
+                        <a href="sgsst.php" class="dropdown-item">SG-SST</a>
+                        <a href="manejoNomina.php" class="dropdown-item">Manejo de nómina y seguridad social</a>
+                        <a href="manejoCliente.php" class="dropdown-item">Manejo de clientes con dificultades, quejas yreclamos</a>
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contáctenos</a>
             </div>
-            <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i class="fa fa-arrow-right ms-3"></i></a>
+            <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block"><i class="fa fa-sign-in ms-3"></i></a>
         </div>
     </nav>

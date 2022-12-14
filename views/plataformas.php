ebd12fe29a7332ac4713fc8eaebbf315fb95ca0b
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2022-12-13 21:21:38 -0500

    version 0

--- views/plataformas.php
+++ views/plataformas.php
@@ -6,7 +6,7 @@
                         <div class="p-4">
                             <img src="/img/Logo_Q10.png" alt="logo Q10" class="w-100">
                             <h5 class="mb-3">Q10 PARA <br>TÉCNICOS</h5>
-                            <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                            <a class="btn btn-primary py-3 px-5 mt-2" href="">ENTRAR AHORA</a>
                         </div>
                     </div>
                 </div>
@@ -15,16 +15,16 @@
                         <div class="p-4">
                         <img src="/img/Logo_Q10.png" alt="logo Q10" class="w-100">
                             <h5 class="mb-3">Q10 PARA BACHILLERATO</h5>
-                            <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                            <a class="btn btn-primary py-3 px-5 mt-2" href="">ENTRAR AHORA</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="service-item text-center pt-3">
                         <div class="p-4">
-                        <img src="/img/campus.png" alt="logo Q10" class="w-100">
+                        <i class="fa-sharp fa-3x fa-solid fa-laptop-code"></i>
                             <h5 class="mb-3">CAMPUS VIRTUAL</h5>
-                            <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                            <a class="btn btn-primary py-3 px-5 mt-2" href="">ENTRAR AHORA</a>
                         </div>
                     </div>
                 </div>
@@ -33,7 +33,7 @@
                         <div class="p-4">
                             <i class="fa fa-3x fa-book-open text-primary mb-4"></i>
                             <h5 class="mb-3">APLICACIONES WEB Y ANDROID</h5>
-                            <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                            <a class="btn btn-primary py-3 px-5 mt-2" href="">ENTRAR AHORA</a>
                         </div>
                     </div>
                 </div>

20d1c245d6c3775d8d16e93d30e4d6c3d218d0fd
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2022-12-13 21:04:45 -0500

    VERSION 0

--- views/plataformas.php
+++ views/plataformas.php
@@ -4,7 +4,7 @@
                 <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="service-item text-center pt-3">
                         <div class="p-4">
-                            <img src="/img/Logo_Q10.png" alt="logo Q10" class="w-25">
+                            <img src="/img/Logo_Q10.png" alt="logo Q10" class="w-50">
                             <h5 class="mb-3">Q10 PARA TÉCNICOS</h5>
                             <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
                         </div>
@@ -13,7 +13,7 @@
                 <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="service-item text-center pt-3">
                         <div class="p-4">
-                        <img src="/img/Logo_Q10.png" alt="logo Q10" class="w-25">
+                        <img src="/img/Logo_Q10.png" alt="logo Q10" class="w-50">
                             <h5 class="mb-3">Q10 PARA BACHILLERATO</h5>
                             <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
                         </div>

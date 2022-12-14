da3b2dea6180937acc75b847584342c87cea6af7
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2022-12-13 22:00:43 -0500

    version 0

--- controller/footer.php
+++ controller/footer.php
@@ -57,20 +57,11 @@
             <div class="copyright">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
-
-                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a><br><br>
-                        Distributed By <a class="border-bottom" href="https://themewagon.com">ThemeWagon</a>
+                    Copyright &copy;
+					<script>document.write(new Date().getFullYear());</script> CENDI Todos los Derechos Reservados
+                        Made By <a class="border-bottom" href="https://agenciaeaglesoftware.com/">Agencia de Desarrollo Eagle Software</a><br><br>
                     </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <div class="footer-menu">
-                            <a href="">Home</a>
-                            <a href="">Cookies</a>
-                            <a href="">Help</a>
-                            <a href="">FQAs</a>
-                        </div>
-                    </div>
+                 
                 </div>
             </div>
         </div>

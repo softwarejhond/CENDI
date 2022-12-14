d2fc1a6977b3cb9d93c64e853afb8a04dae2ee2e
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2022-12-13 21:32:58 -0500

    version 0

--- views/plataformas.php
+++ views/plataformas.php
@@ -6,7 +6,7 @@
                         <div class="p-4">
                             <img src="/img/Logo_Q10.png" alt="logo Q10" class="w-100">
                             <h5 class="mb-3">TÉCNICOS</h5>
-                            <a class="btn btn-primary py-3 px-5 mt-2" href="">ENTRAR AHORA</a>
+                            <a class="btn btn-primary py-3 px-5 mt-2" href="https://site3.q10.com/login?ReturnUrl=%2F&aplentId=d0e037d5-22c3-4082-b55e-d30628819936" target="_blank">ENTRAR AHORA</a>
                         </div>
                     </div>
                 </div>
@@ -15,7 +15,7 @@
                         <div class="p-4">
                         <img src="/img/Logo_Q10.png" alt="logo Q10" class="w-100">
                             <h5 class="mb-3">BACHILLERATO</h5>
-                            <a class="btn btn-primary py-3 px-5 mt-2" href="">ENTRAR AHORA</a>
+                            <a class="btn btn-primary py-3 px-5 mt-2" href="https://site2.q10.com/login?ReturnUrl=%2F&aplentId=c866b1f7-b5a4-4146-a6d5-eda3de6bdea9" target="_blank">ENTRAR AHORA</a>
                         </div>
                     </div>
                 </div>
@@ -24,7 +24,7 @@
                         <div class="p-4">
                         <i class="fa fa-3x fa-laptop text-primary mb-4"></i>
                             <h5 class="mb-3">CAMPUS VIRTUAL</h5>
-                            <a class="btn btn-primary py-3 px-5 mt-2" href="">ENTRAR AHORA</a>
+                            <a class="btn btn-primary py-3 px-5 mt-2" href="https://cendi.kurve.digital/" target="_blank">ENTRAR AHORA</a>
                         </div>
                     </div>
                 </div>
@@ -33,7 +33,7 @@
                         <div class="p-4">
                             <i class="fa fa-3x fa-code text-primary mb-4"></i>
                             <h5 class="mb-3">WEB Y ANDROID</h5>
-                            <a class="btn btn-primary py-3 px-5 mt-2" href="">ENTRAR AHORA</a>
+                            <a class="btn btn-primary py-3 px-5 mt-2" href="https://moodle.cendi.fundacionview.org/" target="_blank">ENTRAR AHORA</a>
                         </div>
                     </div>
                 </div>

23d4cbd37358838ae62e1dd4893bb1c014f3bdbf
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2022-12-13 21:34:39 -0500

    VERSION 0

--- controller/head.php
+++ controller/head.php
@@ -29,4 +29,8 @@
 
     <!-- Template Stylesheet -->
     <link href="css/style.css" rel="stylesheet">
+
+    <!-- Start of HubSpot Embed Code  Chat-->
+	<script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/21270719.js"></script>
+	<!-- End of HubSpot Embed Code Chat -->
 </head>

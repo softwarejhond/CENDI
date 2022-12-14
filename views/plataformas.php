7a40613e07ae7664b570b7dae7eced2bd82e2090
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2022-12-13 21:31:00 -0500

    version 0

--- views/plataformas.php
+++ views/plataformas.php
@@ -31,7 +31,7 @@
                 <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
                     <div class="service-item text-center pt-3">
                         <div class="p-4">
-                            <i class="fa fa-3x fa-android text-primary mb-4"></i>
+                            <i class="fa fa-3x fa-code text-primary mb-4"></i>
                             <h5 class="mb-3">WEB Y ANDROID</h5>
                             <a class="btn btn-primary py-3 px-5 mt-2" href="">ENTRAR AHORA</a>
                         </div>
